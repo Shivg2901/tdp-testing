1bed6a2a2be120424eb27d1e0fca26789255cea4
Shivansh <guptashivansh374@gmail.com>
2025-05-18 11:52:06 +0530

    chore(ui): fix logo size, update favicon and update changelog

--- frontend/theme.config.jsx
+++ frontend/theme.config.jsx
@@ -1,36 +1,29 @@
-import { Image, ImageZoom } from "nextra/components";
+import { Image, ImageZoom } from 'nextra/components';
 
 export default {
   components: {
     img: ({ alt, ...props }) => (
       <>
-        <ImageZoom
-          alt={alt}
-          style={{ borderRadius: "0.5rem", margin: "0 auto" }}
-          {...props}
-        />
-        <i style={{ textAlign: "center", display: "block" }}>{alt}</i>
+        <ImageZoom alt={alt} style={{ borderRadius: '0.5rem', margin: '0 auto' }} {...props} />
+        <i style={{ textAlign: 'center', display: 'block' }}>{alt}</i>
       </>
     ),
   },
   head: (
     <>
-      <meta charSet="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charSet='UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <title>Documentation | Target Discovery Platform</title>
-      <meta
-        name="description"
-        content="Help Manual for Drug Target Discovery Platform for Homosapiens"
-      />
+      <meta name='description' content='Help Manual for Drug Target Discovery Platform for Homosapiens' />
     </>
   ),
   logo: (
     <>
-      <Image src="/image/logo.png" alt="TDP Logo" width={150} height={150} />
-      <span className="ml-2 font-bold">TDP Help Manual</span>
+      <Image src='/image/logo.png' alt='TDP Logo' width={50} height={50} />
+      <span className='ml-2 font-bold'>TDP Help Manual</span>
     </>
   ),
-  logoLink: "/",
+  logoLink: '/',
   darkMode: true,
   nextThemes: {
     defaultTheme: 'light',
